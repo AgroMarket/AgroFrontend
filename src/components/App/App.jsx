49e039f0f72b3666825c0c313b12126ecbd43e18
Alexandr Fokin <sfy@mail.ru>
2018-10-21 00:54:50 +0300

    Подключен сброс CSS для браузеров от Material-UI

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import './App.scss';
 
-import React, { PureComponent } from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Header from 'components/Header';
 
@@ -14,6 +15,8 @@ import routes from '../../routes';
 export default class App extends PureComponent {
   render() {
     return (
+      <Fragment>
+        <CssBaseline/>
         <BrowserRouter>
           <div>
             {/* Header */}
@@ -23,6 +26,7 @@ export default class App extends PureComponent {
             </Switch>
           </div>
         </BrowserRouter>
+      </Fragment>
     );
   }
 }

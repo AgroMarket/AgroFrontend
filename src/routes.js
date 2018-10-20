cb9f342f875f9eb7ec6fc430de26a959518f457c
Alexandr Fokin <sfy@mail.ru>
2018-10-20 21:44:42 +0300

    Добавлены маршруты для главного меню

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,48 @@
 import Home from 'pages/HomePage';
+import About from 'pages/AboutPage';
+import Comments from 'pages/CommentsPage';
+import Delivery from 'pages/DeliveryPage';
+import Help from 'pages/HelpPage';
+import Contacts from 'pages/ContactsPage';
 
 /**
  * Маршрутизация приложения
  */
 export default [
-  // Главная страница блога - Home
+  // Главная (стартовая) страница Ferma Store
   {
     path: '/',
     component: Home,
+    exact: true,
+  },
+  // О нас
+  {
+    path: '/about',
+    component: About,
+    exact: true,
+  },
+  // Отзывы
+  {
+    path: '/comments',
+    component: Comments,
+    exact: true,
+  },
+  // Доставка
+  {
+    path: '/delivery',
+    component: Delivery,
+    exact: true,
+  },
+  // Вопросы и ответы
+  {
+    path: '/help',
+    component: Help,
+    exact: true,
+  },
+  // Контакты
+  {
+    path: '/contacts',
+    component: Contacts,
     exact: true,
   },
 ];

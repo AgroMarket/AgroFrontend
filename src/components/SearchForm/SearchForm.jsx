d0f9f937337a3f327a8261368a7163bc7f00db97
Alexandr Fokin <sfy@mail.ru>
2018-10-28 02:04:04 +0300

    Компонент SearchForm для визуализации строки поиска по каталогу

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,53 @@
+import './SearchForm.scss';
+
+import React, { PureComponent } from 'react';
+// Проверка свойств
+import PropTypes from 'prop-types';
+
+/**
+ * Класс SearchForm - компонент, отображающий на странице строку поиска товара
+ */
+export default class SearchForm extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      // значение поля Товар по умолчанию
+      item: '',
+    };
+  }
+
+  // Проверка свойств
+  static propTypes = {
+    // свойство должно быть функцией
+    onSend: PropTypes.func,
+  };
+
+  // обработка нажатий на клавиши
+  handleChange = event => {
+    this.setState({
+      // подставляем переменную в ключ объекта
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // обработка щелчков по кнопке
+  handleClick = () => {
+    // получаем значение
+    const {onSend} = this.props;
+
+    onSend(this.state);
+  };
+
+  render() {    
+    // получаем содержимое state
+    const { item } = this.state;
+    return (
+      <div>
+        <input type="text" name="item" onChange={this.handleChange} value={item}/>
+        <button onClick={this.handleClick}>Найти</button>
+      </div>
+    );
+  }
+}

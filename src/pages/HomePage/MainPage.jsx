1cf119de697f6228f542d8b74c45a3e72d09ae99
Alexandr Fokin <sfy@mail.ru>
2018-10-30 22:29:22 +0300

    Использована заглушка для данных о пунктах меню каталога

--- src/pages/HomePage/MainPage.jsx
+++ src/pages/HomePage/MainPage.jsx
@@ -6,71 +6,7 @@ import SearchForm from 'components/SearchForm';
 import CatalogList from 'components/CatalogList';
 // TODO удалить mock.json
 import startListData from 'mocks/startlist.json';
-
-// Список пунктов меню
-const menu = [
-  // Главная (стартовая) страница Ferma Store
-  {
-    id: '1',
-    name: 'item1',
-    path: '/item1',
-  },
-  {
-    id: '2',
-    name: 'item2',
-    path: '/item2',
-  },
-  {
-    id: '3',
-    name: 'item3',
-    path: '/item3',
-  },
-  {
-    id: '4',
-    name: 'item4',
-    path: '/item4',
-  },
-  {
-    id: '5',
-    name: 'item5',
-    path: '/item5',
-  },
-  {
-    id: '6',
-    name: 'item6',
-    path: '/item6',
-  },
-  {
-    id: '7',
-    name: 'item7',
-    path: '/item7',
-  },
-  {
-    id: '8',
-    name: 'item8',
-    path: '/item8',
-  },
-  {
-    id: '9',
-    name: 'item9',
-    path: '/item9',
-  },
-  {
-    id: '10',
-    name: 'item10',
-    path: '/item10',
-  },
-  {
-    id: '11',
-    name: 'item11',
-    path: '/item11',
-  },
-  {
-    id: '12',
-    name: 'item12',
-    path: '/item12',
-  },
-];
+import catalogMenuData from 'mocks/catalogmenu.json';
 
 /**
  * Класс HomePage - компонент, отображающий главную страницу
@@ -80,29 +16,46 @@ export default class MainPage extends PureComponent {
     super(props);
 
     this.state = {
-      items: [],
-      isLoaded: false,
+      menuItems: [],
+      catalogItems: [],
+      menuLoaded: false,
+      itemsLoaded: false,
       error: null,
     };
   }
 
   componentDidMount() {
     // TODO включить прием данных с сервера
-/*    fetch('http://80.211.153.183:3000/api/startlist')
+/*    fetch('http://80.211.153.183:3000/api/catalogmenu')
       .then(res => res.json())
       .then(res => {
-        this.setState({items: res});
-        this.setState({isLoaded: true});
+        this.setState({menuItems: res});
+        this.setState({menuLoaded: true});
         },
       error => {
         this.setState({
-          isLoaded: true,
+          menuLoaded: true,
+          error,
+        });
+      });
+
+      fetch('http://80.211.153.183:3000/api/startlist')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({catalogItems: res});
+        this.setState({itemsLoaded: true});
+        },
+      error => {
+        this.setState({
+          itemsLoaded: true,
           error,
         });
       });*/
     // TODO удалить mock.json
-    this.setState({items: startListData});
-    this.setState({isLoaded: true});
+    this.setState({menuItems: catalogMenuData});
+    this.setState({menuLoaded: true});
+    this.setState({catalogItems: startListData});
+    this.setState({itemsLoaded: true});
   }
 
   handleItemSearch = items => {
@@ -111,30 +64,30 @@ export default class MainPage extends PureComponent {
         return {
           ...prevState,
           // возвращаем массив найденных товаров
-          items: items,
+          catalogItems: items,
         };
       }
     );
   };
 
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, itemsLoaded, menuLoaded, menuItems, catalogItems } = this.state;
     if (error) {
       return <p>Ошибка: {error.message}</p>;
     }
     else
     // Отображаем main
-      if (!isLoaded) {
+      if (!itemsLoaded || !menuLoaded) {
         return <p>Пожалуйста, подождите, идет загрузка страницы</p>;
       }
       else {
         return (
           <main>
             <div/>
-            <LeftMenu menu={menu} className="left_menu"/>
+            <LeftMenu menu={menuItems} className="left_menu"/>
             <div>
               <SearchForm onSend={this.handleItemSearch}/>
-              <CatalogList items={items}/>
+              <CatalogList items={catalogItems}/>
             </div>
             <div/>
           </main>

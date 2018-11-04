62b1bc955c11e9519a37505d8ee5ff273f8e9c9f
Alexandr Fokin <sfy@mail.ru>
2018-11-05 00:34:24 +0300

    Подключена пользовательская тема для отображения компонентов Material-UI

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,17 @@ import './index.scss';
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+// Тема для Material-UI
+import { theme} from './theme';
 
 import App from 'components/App';
 
 // Передаем корневой компонент App в DOM, инициализируем и визуализируем все дерево компонентов
 ReactDom.render(
-  // отрисовываемый JSX
-  <App/>,
+  <MuiThemeProvider theme = { theme }>
+    <App/>
+  </MuiThemeProvider>,
   // привязываем react-приложение к HTML
   document.getElementById('root')
 );

8b23200c940c8f29b2c61337896dbf6d7ec686d9
Alexandr Fokin <sfy@mail.ru>
2018-11-05 20:40:39 +0300

    Применил изменения в дизайне макета главной страницы

--- src/components/LeftMenu/LeftMenu.jsx
+++ src/components/LeftMenu/LeftMenu.jsx
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
 
 /**
  * Класс LeftMenu - компонент, отображающий разделы каталога на странице каталога
@@ -69,7 +68,6 @@ export default class LeftMenu extends PureComponent {
               >
                 <ListItemText primary={item.name}/>
               </ListItem>
-              <Divider />
             </Fragment>
           );
         })}

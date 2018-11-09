d2646e07243fcde662db7ed695427e69fd661c50
Alexandr Fokin <sfy@mail.ru>
2018-11-09 20:29:24 +0300

    По нажатию на Корзина открывается корзина

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,23 @@ import React, {PureComponent} from 'react';
 
 import MainMenu from 'components/MainMenu';
 import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
 
+const linkToBasket = props => <Link to="/basket" {...props}/>;
 // Данные для кнопки Корзина
-const basketButton = {id: 'basket', name: 'Корзина'};
+const basketButton = {
+  id: 'basket',
+  name: 'Корзина',
+};
 // Данные для кнопки Вход
-const loginButton = {id: 'login', name: 'Вход'};
+const loginButton = {
+  id: 'login',
+  name: 'Вход',
+};
 
 /**
  * Класс Header - компонент, отображающий хидер страницы
+ * TODO сделать снятие выделения последнего открытого пункта меню в хидере
  */
 export default class Header extends PureComponent {
   render() {
@@ -22,16 +31,19 @@ export default class Header extends PureComponent {
         <div className="menu_buttons">
           <span className="basket_button">
             <Button
+              component={linkToBasket}
               variant="contained"
               color="primary"
-              id={basketButton.id}>
+              id={basketButton.id}
+            >
                 {basketButton.name}
             </Button>
           </span>
           <span className="login_button">
             <Button
               color="primary"
-              id={loginButton.id}>
+              id={loginButton.id}
+            >
                 {loginButton.name}
             </Button>
           </span>

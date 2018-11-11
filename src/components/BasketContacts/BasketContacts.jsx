1eebf38a3fb4e121d8497323a7ae1f1012e7561c
Alexandr Fokin <sfy@mail.ru>
2018-11-11 03:45:19 +0300

    Каркас компонента для контактных данных в корзине

---
+++ src/components/BasketContacts/BasketContacts.jsx
@@ -0,0 +1,41 @@
+import './BasketContacts.scss';
+
+import React, { PureComponent } from 'react';
+import Button from '@material-ui/core/Button/Button';
+
+// Данные для кнопки Оформить заказ
+const sendOrderButton = {id: 'sendOrder', name: 'Оформить заказ'};
+
+/**
+ * Класс BasketContacts - компонент, отображающий форму с контактными данными на странице Корзина
+ */
+export default class BasketContacts extends PureComponent {
+  render() {
+    return (
+      <div className="basket_contacts">
+        <p className="description">
+          Чтобы продолжить, заполните свои данные
+        </p>
+        <input type="text" id="label_fio" name="contact_fio" placeholder=' '/>
+        <label className="contact_fio" htmlFor="label_fio">Фамилия Имя Отчество</label>
+        <input type="text" id="label_city" name="contact_city" placeholder=' '/>
+        <label className="contact_city" htmlFor="label_city">Город</label>
+        <input type="text" id="label_address" name="contact_address" placeholder=' '/>
+        <label className="contact_address" htmlFor="label_address">Адрес доставки</label>
+        <input type="text" id="label_phone" name="contact_phone" placeholder=' '/>
+        <label className="contact_phone" htmlFor="label_phone">Телефон</label>
+        <input type="text" id="label_email" name="contact_email" placeholder=' '/>
+        <label className="contact_email" htmlFor="label_email">Адрес электронной почты</label>
+        <input type="text" id="label_password" name="contact_password" placeholder=' '/>
+        <label className="contact_password" htmlFor="label_password">Пароль</label>
+        <Button
+          className="send_order_button"
+          variant="contained"
+          color="primary"
+        >
+          {sendOrderButton.name}
+        </Button>
+      </div>
+    );
+  }
+}

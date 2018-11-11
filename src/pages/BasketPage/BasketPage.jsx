be196dddcc4dcd6e2cff8d1bcca2c3c4ac190f30
Alexandr Fokin <sfy@mail.ru>
2018-11-11 15:15:47 +0300

    Отображение сведений о загрузке данных с сервера

--- src/pages/BasketPage/BasketPage.jsx
+++ src/pages/BasketPage/BasketPage.jsx
@@ -4,26 +4,29 @@ import React, { PureComponent } from 'react';
 import BasketList from 'components/BasketList';
 import BasketContacts from 'components/BasketContacts';
 import PropTypes from 'prop-types';
+import {serverAddress} from 'constants/ServerAddress';
 
-const basketItems = {
-  'products': [
-    {
-      'cart_item_id': 1,
-      'product_name': 'Квас №6',
-      'product_price': 30,
-      'product_quantity': 2,
-      'product_id': 276,
-      'product_link': '/api/products/276',
-    },
-    {
-      'cart_item_id': 2,
-      'product_name': 'Сиропы №1',
-      'product_price': 70,
-      'product_quantity': 1,
-      'product_id': 277,
-      'product_link': '/api/products/277',
-    },
-  ],
+const basketItemsJSON = {
+  'result': {
+    'products': [
+      {
+        'cart_item_id': 1,
+        'product_name': 'Квас №6',
+        'product_price': 30,
+        'product_quantity': 2,
+        'product_id': 276,
+        'product_link': '/api/products/276',
+      },
+      {
+        'cart_item_id': 2,
+        'product_name': 'Сиропы №1',
+        'product_price': 70,
+        'product_quantity': 1,
+        'product_id': 277,
+        'product_link': '/api/products/277',
+      },
+    ],
+  },
 };
 /**
  * Класс BasketPage - компонент, отображающий страницу Корзина
@@ -48,15 +51,38 @@ export default class BasketPage extends PureComponent {
     basketID: PropTypes.number,
   };
 
-  render() {
-    const { basketID } = this.props;
-    return (
-      <div className="basket_form">
-        <div/>
-        <BasketList basketItems={basketItems.products} basketID={basketID}/>
-        <BasketContacts basketID={basketID}/>
-        <div/>
-      </div>
+  componentDidMount() {
+    // TODO сделать запрос с сервера
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          basketItems: basketItemsJSON.result.products,
+          basketLoaded: true,
+        };
+      }
     );
+  }
+
+  render() {
+    const { error, basketItems, basketLoaded } = this.state;
+    const { basketID } = this.props;
+    if (error) {
+      return <p>Ошибка: {error.message}</p>;
+    }
+    else
+    if (!basketLoaded) {
+      return <p className="load_info">Пожалуйста, подождите, идет загрузка страницы</p>;
+    }
+    else {
+      return (
+        <div className="basket_form">
+          <div/>
+          <BasketList basketItems={basketItems} basketID={basketID}/>
+          <BasketContacts basketID={basketID}/>
+          <div/>
+        </div>
+      );
+    }
   }
 }

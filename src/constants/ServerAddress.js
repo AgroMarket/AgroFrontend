6a4fa84116c89a6e40a23e08ca44b7240e99a909
Alexandr Fokin <sfy@mail.ru>
2018-11-12 17:47:43 +0300

    переключил на старый сервер для бекэнда

--- src/constants/ServerAddress.js
+++ src/constants/ServerAddress.js
@@ -1,6 +1,6 @@
 // Адрес сервера для бекэнда
-export const serverAddress = 'https://nameless-tundra-78857.herokuapp.com';
+// export const serverAddress = 'https://nameless-tundra-78857.herokuapp.com';
 // Старый адрес сервера для бекэнда
-// export const serverAddress = 'http://80.211.153.183:3000';
+export const serverAddress = 'http://80.211.153.183:3000';
 // Адрес mock-сервера
 //export const serverAddress = 'http://localhost:80';

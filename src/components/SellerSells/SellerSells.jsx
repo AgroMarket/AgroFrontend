b9f7c5acce17aedc066ddd457a9f52696786100f
Alexandr Fokin <sfy@mail.ru>
2018-11-17 00:16:22 +0300

    Заглушка компонента для клиентских заказов продавца

---
+++ src/components/SellerSells/SellerSells.jsx
@@ -0,0 +1,23 @@
+import './SellerSells.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс SellerSells - компонент, отображающий ****** на странице
+ */
+export default class SellerSells extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>SellerSells</p>
+    );
+  }
+}

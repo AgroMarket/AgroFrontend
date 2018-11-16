7378503b5d00dd9eb4ebab06c0f95aba38d2f6b0
Alexandr Fokin <sfy@mail.ru>
2018-11-17 00:19:07 +0300

    Заглушка для компонента с клиентами продавца

---
+++ src/components/SellerClients/SellerClients.jsx
@@ -0,0 +1,23 @@
+import './SellerClients.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс SellerClients - компонент, отображающий ****** на странице
+ */
+export default class SellerClients extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>SellerClients</p>
+    );
+  }
+}

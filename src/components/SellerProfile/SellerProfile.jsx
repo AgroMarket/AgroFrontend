6a4869519d7a7d869179562c520a04458d43cf5d
Alexandr Fokin <sfy@mail.ru>
2018-11-17 00:16:45 +0300

    Заглушка для компонента с профилем продавца

---
+++ src/components/SellerProfile/SellerProfile.jsx
@@ -0,0 +1,23 @@
+import './SellerProfile.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс SellerProfile - компонент, отображающий ****** на странице
+ */
+export default class SellerProfile extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>SellerProfile</p>
+    );
+  }
+}

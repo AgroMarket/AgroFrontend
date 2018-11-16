f70999d2d7f7580e62fc67029a7fc399d078e9b0
Alexandr Fokin <sfy@mail.ru>
2018-11-17 00:17:46 +0300

    Заглушка для компонента с товарами продавца

---
+++ src/components/SellerItems/SellerItems.jsx
@@ -0,0 +1,23 @@
+import './SellerItems.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс SellerItems - компонент, отображающий ****** на странице
+ */
+export default class SellerItems extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>SellerItems</p>
+    );
+  }
+}

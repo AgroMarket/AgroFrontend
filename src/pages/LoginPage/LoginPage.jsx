bb339c3dbb1cfda87b2a5a83c3676d58b3bc4f84
Alexandr Fokin <sfy@mail.ru>
2018-11-17 20:51:46 +0300

    Страница входа

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,23 @@
+import './LoginPage.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс LoginPage - компонент, отображающий страницу авторизации
+ */
+export default class LoginPage extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>LoginPage</p>
+    );
+  }
+}

d17d97244b52b5153c114a1848b72f091360b83f
Alexandr Fokin <sfy@mail.ru>
2018-11-18 21:00:06 +0300

    компонент, отображающий сведения о покурателе на странице продавца

---
+++ src/components/SellerClient/SellerClient.jsx
@@ -0,0 +1,70 @@
+import './SellerClient.scss';
+
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button/Button';
+
+// TODO заменить на ссылку изображения с сервера
+import PhotoMock from 'img/nophoto.jpg';
+
+// Данные для кнопки Посмотреть профиль
+const editItemButton = {
+  id: 'open_client_info',
+  name: 'Посмотреть профиль',
+};
+
+/**
+ * Класс SellerClient - компонент, отображающий сведения о покурателе на странице продавца
+ */
+export default class SellerClient extends PureComponent {
+// Проверка свойств
+  static propTypes = {
+    // Пункты меню - массив объектов
+    item: PropTypes.shape({
+      // имя покупателя
+      name: PropTypes.string,
+      // почта  покупателя
+      email: PropTypes.string,
+      // телефон  покупателя
+      phone: PropTypes.string,
+      // адресс  покупателя
+      address: PropTypes.string,
+      // изображение покупателя
+      image: PropTypes.string,
+      // ссылка на покупателя
+      link: PropTypes.string,
+    }),
+  };
+  
+  render() {
+    const { item } = this.props;
+
+    return (
+      <p className="seller_item client_info">
+        <img
+          src={PhotoMock}
+          alt="Клиент"
+        />
+        <span className="consumer_info">
+          <span className="consumer_name">
+            Покупатель: {item.consumer.name}
+          </span>
+          <span className="consumer_address">
+            Адрес: {item.consumer.address}
+          </span>
+          <span className="consumer_phone">
+            Телефон: +7-{item.consumer.phone}
+          </span>
+        </span>
+        <Button
+          className="edit_button"
+          variant="contained"
+          color="primary"
+          id={editItemButton.id}
+        >
+          {editItemButton.name}
+        </Button>
+      </p>
+    );
+  }
+}

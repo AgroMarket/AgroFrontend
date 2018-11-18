a596cf164896c721b6ca89167d39e28668d3c4b0
Alexandr Fokin <sfy@mail.ru>
2018-11-18 23:33:50 +0300

    Заглушка для профиля продавца

--- src/components/SellerProfile/SellerProfile.jsx
+++ src/components/SellerProfile/SellerProfile.jsx
@@ -1,9 +1,36 @@
 import './SellerProfile.scss';
 
 import React, { PureComponent } from 'react';
+import Button from '@material-ui/core/Button/Button';
+
+// Данные для кнопки Редактировать профиль
+const editProfileButton = {
+  id: 'open_profile',
+  name: 'Редактировать профиль',
+};
+
+// TODO заменить заглушку с товарами на данные с сервера
+const profileJSON = {
+  'status': 200,
+  'message': 'Профиль производителя',
+  'result': {
+    'profile': {
+      'id': 7,
+      'brand': 'Farmer2',
+      'address': 'Владикавказ',
+      'phome': '450-296-7653',
+      'inn': '150072240',
+      'descripion': 'Next level yr DIY +1 blog. Readymade quinoa banh mi mlkshk craft beer iPhone. Craft beer Banksy mlkshk etsy art you probably havent heard of them vice Shoreditch.',
+      'logo': '',
+      'sertificates': [],
+      'link': '/api/producers/7',
+    },
+  },
+  'error': null,
+};
 
 /**
- * Класс SellerProfile - компонент, отображающий ****** на странице
+ * Класс SellerProfile - компонент, отображающий профиль на странице продавца
  */
 export default class SellerProfile extends PureComponent {
   constructor(props) {
@@ -11,13 +38,41 @@ export default class SellerProfile extends PureComponent {
     
     // значения полей, используемых в render()
     this.state = {
-      
+      // данные профиля
+      profile: profileJSON.result,
     };
   }
   
   render() {
+    const { profile } = this.state;
     return (
-      <p>SellerProfile</p>
+      <div className="seller_profile">
+        <span className="profile_info">
+          <span className="profile_name">
+            {profile.profile.brand}
+          </span>
+          <span className="profile_address">
+            Регион: {profile.profile.address}
+          </span>
+          <span className="profile_phone">
+            Телефон: {profile.profile.phome}
+          </span>
+          <span className="profile_inn">
+            ИНН: {profile.profile.inn}
+          </span>
+          <span className="profile_description">
+            {profile.profile.descripion}
+          </span>
+          <Button
+            className="edit_profile"
+            variant="contained"
+            color="primary"
+            id={editProfileButton.id}
+          >
+          {editProfileButton.name}
+        </Button>
+        </span>
+      </div>
     );
   }
 }

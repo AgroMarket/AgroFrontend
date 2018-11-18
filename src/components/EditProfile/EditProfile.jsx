eaac3e9e1320e19722f1e9ea4437e22e3e56006c
Alexandr Fokin <sfy@mail.ru>
2018-11-19 00:42:52 +0300

    компонент, отображающий форму редактирования профиля продавца на странице продавца

---
+++ src/components/EditProfile/EditProfile.jsx
@@ -0,0 +1,23 @@
+import './EditProfile.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс EditProfile - компонент, отображающий форму редактирования профиля продавца на странице продавца
+ */
+export default class EditProfile extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>EditProfile</p>
+    );
+  }
+}

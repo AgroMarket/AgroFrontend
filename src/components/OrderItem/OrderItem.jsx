f1cd0d2c54e61faea22f174cb18020ae719d05f1
Alexandr Fokin <sfy@mail.ru>
2018-11-18 03:12:42 +0300

    Компонент для отображения строки с информацией о заказе

---
+++ src/components/OrderItem/OrderItem.jsx
@@ -0,0 +1,59 @@
+import './OrderItem.scss';
+
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button/Button';
+import moment from 'moment'
+
+// Данные для кнопки Открыть заказ
+const openOrderButton = {
+  id: 'open_order',
+  name: 'Открыть заказ',
+};
+
+/**
+ * Класс OrderItem - компонент, отображающий строку со сведениями о заказе на странице продавца
+ */
+export default class OrderItem extends PureComponent {
+  // Проверка свойств
+  static propTypes = {
+    // Пункты меню - массив объектов
+    item: PropTypes.shape({
+      // время заказа
+      date: PropTypes.string,
+      // общая стоимость заказа
+      total: PropTypes.string,
+      // состояние заказа
+      status: PropTypes.string,
+      // ссылка на заказ
+      link: PropTypes.string,
+    }),
+  };
+
+  render() {
+    const { item } = this.props;
+    const date = moment(item.order.date).format('DD.MM.YY HH:MM');
+
+    return (
+      <p className="seller_item">
+        <span className="order_date">
+          {date}
+        </span>
+        <span className="order_total">
+          на сумму {item.order.total} руб.
+        </span>
+        <span className="order_status">
+          {item.order.status}
+        </span>
+        <Button
+          className="edit_button"
+          variant="contained"
+          color="primary"
+          id={openOrderButton.id}
+        >
+          {openOrderButton.name}
+        </Button>
+      </p>
+    );
+  }
+}

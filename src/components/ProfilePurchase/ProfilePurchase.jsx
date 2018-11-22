93cefc5b89932c6da1b23db20a3ef186633b5a5b
Alexandr Fokin <sfy@mail.ru>
2018-11-22 22:46:20 +0300

    компонент, отображающий Покупки на странице личного кабинета

---
+++ src/components/ProfilePurchase/ProfilePurchase.jsx
@@ -0,0 +1,23 @@
+import './ProfilePurchase.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс ProfilePurchase - компонент, отображающий Покупки на странице личного кабинета
+ */
+export default class ProfilePurchase extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>ProfilePurchase</p>
+    );
+  }
+}

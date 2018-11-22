4c89e6fa46e052f8160a2f1839d1fb5da1ddda91
Alexandr Fokin <sfy@mail.ru>
2018-11-22 22:50:11 +0300

    компонент, отображающий список продавцов на странице личного кабинета

---
+++ src/components/ProfileSellers/ProfileSellers.jsx
@@ -0,0 +1,23 @@
+import './ProfileSellers.scss';
+
+import React, { PureComponent } from 'react';
+
+/**
+ * Класс ProfileSellers - компонент, отображающий список продавцов на странице личного кабинета
+ */
+export default class ProfileSellers extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    // значения полей, используемых в render()
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <p>ProfileSellers</p>
+    );
+  }
+}

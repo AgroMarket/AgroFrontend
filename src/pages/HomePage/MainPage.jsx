f151d28efd1a60dff08553fd4d988ff0b4edf99b
Dmitry Fursenko <dfursenko@gmail.com>
2018-11-26 19:19:21 +0700

    add CatalogProducer in HomePage

--- src/pages/HomePage/MainPage.jsx
+++ src/pages/HomePage/MainPage.jsx
@@ -5,6 +5,7 @@ import LeftMenu from 'components/LeftMenu';
 import SearchForm from 'components/SearchForm';
 import CatalogList from 'components/CatalogList';
 import CatalogItem from 'components/CatalogItem';
+import CatalogProducer from 'components/CatalogProducer';
 import {serverAddress} from 'constants/ServerAddress';
 import PropTypes from 'prop-types';
 
@@ -24,6 +25,8 @@ export default class MainPage extends PureComponent {
       openedSection: '/api/products?scope=samples',
       // адрес открытого товара каталога
       openedItem: '',
+      // адрес производителя
+      openedProducer: '',
       // ошибка загрузки
       error: null,
       // TODO добавить пагинацию для вывода товаров каталога
@@ -110,6 +113,23 @@ export default class MainPage extends PureComponent {
     );
   };
 
+  // Пользователь открывает страницу производителя
+  openProducer = (event, producerID) => {
+
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          openedProducer: `/api/producers/${producerID}`,
+          // openedItem: `/api/producers/143`,
+
+          // переходим в режим отображения Страницы производителя
+          mode: 'catalogProducer',
+        };
+      }
+    );
+  };
+
   // Пользователь возвращается в каталог из просмотра карточки товара
   closeItem = () => {
     this.setState(
@@ -125,7 +145,7 @@ export default class MainPage extends PureComponent {
   };
 
   render() {
-    const { error, menuLoaded, menuItems, openedSection, mode, openedItem, searchResults } = this.state;
+    const { error, menuLoaded, menuItems, openedSection, mode, openedItem, searchResults, openedProducer } = this.state;
     const { basketID } = this.props;
     if (error) {
       return <p>Ошибка: {error.message}</p>;
@@ -140,7 +160,10 @@ export default class MainPage extends PureComponent {
           content = <CatalogList section={openedSection} itemHandle={this.openItem}/>;
         }
         if (mode === 'catalogItem') {
-          content = <CatalogItem item={openedItem} actionBack={this.closeItem} basketID={basketID}/>;
+          content = <CatalogItem item={openedItem} actionBack={this.closeItem} producerHandle={this.openProducer} basketID={basketID}/>;
+        }
+        if (mode === 'catalogProducer') {
+          content = <CatalogProducer producerLink={openedProducer} actionBack={this.closeItem} basketID={basketID}/>;
         }
         return (
           <div className="main_page">

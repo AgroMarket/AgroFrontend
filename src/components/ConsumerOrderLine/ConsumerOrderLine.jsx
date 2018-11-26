2a914e569a217016cb4af5eac3f47b6aa1ef5dea
Dmitry Fursenko <dfursenko@gmail.com>
2018-11-26 16:11:00 +0700

    add ConsumerOrderLine component

---
+++ src/components/ConsumerOrderLine/ConsumerOrderLine.jsx
@@ -0,0 +1,65 @@
+import './ConsumerOrderLine.scss';
+
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button/Button';
+import moment from 'moment';
+
+// Данные для кнопки Открыть заказ
+const openOrderButton = {
+  id: 'open_order',
+  name: 'Открыть заказ',
+};
+
+/**
+ * Класс ConsumerOrder - компонент, отображающий строку со сведениями о заказе на странице покупателя
+ */
+export default class ConsumerOrder extends PureComponent {
+  // Проверка свойств
+  static propTypes = {
+    // Пункты меню - массив объектов
+    item: PropTypes.shape({
+      // время заказа
+      date: PropTypes.string,
+      // общая стоимость заказа
+      total: PropTypes.string,
+      // состояние заказа
+      status: PropTypes.string,
+      // ссылка на заказ
+      link: PropTypes.string,
+    }),
+    // Функция отображения сведений о заказе
+    itemHandle: PropTypes.func,
+  };
+
+  render() {
+    const { item, itemHandle } = this.props;
+    const date = moment(item.order.date).format('DD.MM.YY HH:mm');
+
+    return (
+      <p className="seller_item">
+        <span className="order_date">
+          {date}
+        </span>
+        <span>
+          Производитель {item.order.producer}
+        </span>
+        <span className="order_total">
+          Заказ на сумму {item.order.total} руб.
+        </span>
+        <Button
+          className="edit_button"
+          variant="contained"
+          color="primary"
+          id={openOrderButton.id}
+          onClick={() => itemHandle('consumer_order_detail')}
+        >
+          {openOrderButton.name}
+        </Button>
+        <span className="order_status">
+          Состояние: {item.order.status}
+        </span>
+      </p>
+    );
+  }
+}

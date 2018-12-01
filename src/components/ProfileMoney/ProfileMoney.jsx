aefa355d314485ffd4eba1d05cd37c879277e908
Alexandr Fokin <sfy@mail.ru>
2018-12-02 01:48:45 +0300

    Статическая версия компонента для пополнения счета пользователя на сайте

--- src/components/ProfileMoney/ProfileMoney.jsx
+++ src/components/ProfileMoney/ProfileMoney.jsx
@@ -1,6 +1,15 @@
 import './ProfileMoney.scss';
 
 import React, { PureComponent } from 'react';
+import MyOrdersIcon from '@material-ui/icons/DateRange';
+import Button from '@material-ui/core/Button/Button';
+import PropTypes from 'prop-types';
+
+// Данные для кнопки Пополнить счет
+const sendMoneyButton = {
+  id: 'send_money',
+  name: 'Пополнить счет',
+};
 
 /**
  * Класс ProfileMoney - компонент, отображающий форму для пополнения счета на странице личного кабинета
@@ -8,16 +17,122 @@ import React, { PureComponent } from 'react';
 export default class ProfileMoney extends PureComponent {
   constructor(props) {
     super(props);
-    
+
     // значения полей, используемых в render()
     this.state = {
-      
+      name: '',
+      cardNumber: '',
+      dayExpire: '',
+      ccv: '',
+      amount: '',
     };
   }
-  
+
+  static propTypes = {
+    itemHandle: PropTypes.func,
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // TODO отправить money на сервер, когда будет готов api бэкенда
+  sendMoney = (itemID) => {
+    const { itemHandle } = this.props;
+    itemHandle(itemID);
+  };
+
   render() {
+    const { name, cardNumber, dayExpire, ccv, amount } = this.state;
+
     return (
-      <p>ProfileMoney</p>
+      <div className="details_for_pay seller_items">
+        <div className="seller_items_header">
+          <MyOrdersIcon className="my_orders_icon"/>
+          <h2>Пополнение счета на Ferma Store</h2>
+        </div>
+        <input
+          type="text"
+          id="label_name"
+          name="name"
+          placeholder=" "
+          value={name}
+          onChange={this.handleChange}
+        />
+        <label
+          className="item_name"
+          htmlFor="label_name"
+        >
+          Имя держателя карты
+        </label>
+        <input
+          type="text"
+          id="label_card_number"
+          name="cardNumber"
+          placeholder=" "
+          value={cardNumber}
+          onChange={this.handleChange}
+        />
+        <label
+          className="item_card_number"
+          htmlFor="label_card_number"
+        >
+          Номер карты
+        </label>
+        <input
+          type="text"
+          id="label_day_expire"
+          name="dayExpire"
+          placeholder=" "
+          value={dayExpire}
+          onChange={this.handleChange}
+        />
+        <label
+          className="item_day_expire"
+          htmlFor="label_day_expire"
+        >
+          Срок действия
+        </label>
+        <input
+          type="text"
+          id="label_ccv"
+          name="ccv"
+          placeholder=" "
+          value={ccv}
+          onChange={this.handleChange}
+        />
+        <label
+          className="item_ccv"
+          htmlFor="label_ccv"
+        >
+          CCV
+        </label>
+        <input
+          type="text"
+          id="label_amount"
+          name="amount"
+          placeholder=" "
+          value={amount}
+          onChange={this.handleChange}
+        />
+        <label
+          className="item_amount"
+          htmlFor="label_amount"
+        >
+          Сумма для зачисления на счет
+        </label>
+        <Button
+          className="save_item"
+          variant="contained"
+          color="primary"
+          id={sendMoneyButton.id}
+          onClick={() => this.sendMoney('profile_account')}
+        >
+          {sendMoneyButton.name}
+        </Button>
+      </div>
     );
   }
 }

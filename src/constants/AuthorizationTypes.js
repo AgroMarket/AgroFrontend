cb7fbf8d463c11190c2525cf93d2a46334acc8ad
Alexandr Fokin <sfy@mail.ru>
2018-12-06 20:37:16 +0300

    добавлены виды прав доступа для пользователей

--- src/constants/AuthorizationTypes.js
+++ src/constants/AuthorizationTypes.js
@@ -1,6 +1,8 @@
 // Пользователь обладает правами администрации Ferma Store
-export const admin = 'Admin';
+export const admin = 'admin';
 // Пользователь обладает правами продавца
 export const seller = 'Producer';
 // Пользователь обладает правами покупателя
 export const buyer = 'Consumer';
+// Пользователь обладает правами службы доставки
+export const delivery = 'delivery';

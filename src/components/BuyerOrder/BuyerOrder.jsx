ababeab518429a4f23cad48eb8f48561512006dd
Alexandr Fokin <sfy@mail.ru>
2018-12-07 01:11:47 +0300

    статическая версия

--- src/components/BuyerOrder/BuyerOrder.jsx
+++ src/components/BuyerOrder/BuyerOrder.jsx
@@ -1,11 +1,24 @@
 import './BuyerOrder.scss';
 
-import React, { PureComponent } from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import MyOrdersIcon from '@material-ui/icons/DateRange';
 import moment from 'moment';
 
 import {serverAddress} from 'constants/ServerAddress';
+import Button from '@material-ui/core/Button/Button';
+import OrderStatus from 'components/OrderStatus';
+
+// Данные для кнопки Подтвердить получение заказа
+const orderReceivedButton = {
+  id: 'order_received',
+  name: 'Подтвердить получение заказа',
+};
+// Данные для кнопки Отменить заказ
+const orderCancelButton = {
+  id: 'order_delete',
+  name: 'Отменить заказ',
+};
 
 /**
  * Класс BuyerOrder - компонент, отображающий подробные сведения о заказе на странице покупателя
@@ -18,17 +31,23 @@ export default class BuyerOrder extends PureComponent {
     this.state = {
       order: {},
       itemsLoaded: false,
+      orderStatus: '',
     };
   }
 
   // Проверка свойств
   static propTypes = {
     id: PropTypes.number,
+    jwtToken: PropTypes.string,
   };
 
   componentDidMount() {
-    const { id } = this.props;
-    fetch(`${serverAddress}/api/consumer/asks/${id}`)
+    const { id, jwtToken } = this.props;
+    fetch(`${serverAddress}/api/consumer/asks/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${jwtToken}`,
+      },
+    })
       .then(res => res.json())
       .then(res => {
           this.setState(
@@ -37,6 +56,7 @@ export default class BuyerOrder extends PureComponent {
                 ...prevState,
                 order: res.result.ask,
                 itemsLoaded: true,
+                orderStatus: res.result.ask.status,
               };
             }
           );
@@ -49,8 +69,46 @@ export default class BuyerOrder extends PureComponent {
         });
   }
 
+  orderDone = () => {
+    const { id, jwtToken } = this.props;
+
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          orderStatus: 'Обработка данных',
+        };
+      }
+    );
+
+    fetch(`${serverAddress}/api/consumers/asks/${id}`, {
+      method: 'put',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${jwtToken}`,
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+          this.setState(
+            prevState => {
+              return {
+                ...prevState,
+                orderStatus: res.result.ask.status,
+              };
+            }
+          );
+        },
+        error => {
+          this.setState({
+            error,
+          });
+        });
+  };
+
   render() {
-    const {error, order, itemsLoaded} = this.state;
+    const { error, order, itemsLoaded, orderStatus } = this.state;
     const rub = ' руб.';
     moment.locale('ru');
 
@@ -61,13 +119,42 @@ export default class BuyerOrder extends PureComponent {
       return <p className="load_info">Пожалуйста, подождите, идет загрузка страницы</p>;
     }
     else {
-      // TODO рефакторинг доделать
+      let buttons = '';
+      // TODO узнать какой статус у исполненного заказа
+      if (orderStatus !== 'Получен')
+      {
+        buttons = <Fragment>
+          <p>
+            <Button
+              className="orderDone"
+              variant="contained"
+              color="primary"
+              id={orderReceivedButton.id}
+              onClick={() => this.orderDone()}
+            >
+              {orderReceivedButton.name}
+            </Button>
+          </p>
+          <p>
+            <Button
+              className="orderDone"
+              variant="contained"
+              color="primary"
+              id={orderCancelButton.id}
+            >
+              {orderCancelButton.name}
+            </Button>
+          </p>
+        </Fragment>;
+      }
       return (
         <div className="seller_items order_info">
           <div className="seller_items_header">
             <MyOrdersIcon className="my_orders_icon"/>
             <h2>Заказ № {order.id} от {moment(order.date).format('LL')}</h2>
           </div>
+          <OrderStatus orderStatus={orderStatus}/>
+          {buttons}
           <div className="product seller_item">
             <div>
               <span>Название продукта</span>

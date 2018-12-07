bc185db4058b0cadb6e46c466a1c4ceb96973fea
Alexandr Fokin <sfy@mail.ru>
2018-12-07 18:52:41 +0300

    Разбивка заказов по статусу заказа с пагинацией

--- src/components/ProfileSells/ProfileSells.jsx
+++ src/components/ProfileSells/ProfileSells.jsx
@@ -1,7 +1,14 @@
 import './ProfileSells.scss';
 
-import React, { PureComponent } from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import MyOrdersIcon from '@material-ui/icons/DateRange';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPage from '@material-ui/icons/FirstPage';
+import PrevPage from '@material-ui/icons/ChevronLeft';
+import NextPage from '@material-ui/icons/ChevronRight';
+import LastPage from '@material-ui/icons/LastPage';
 import PropTypes from 'prop-types';
 
 import OrderItem from 'components/OrderItem';
@@ -19,6 +26,16 @@ export default class ProfileSells extends PureComponent {
       // Заказы
       orders: {},
       itemsLoaded: false,
+      // номер открытой вкладки
+      value: 0,
+      // текущая страница
+      currentPage: 1,
+      // последняя страница
+      lastPage: 1,
+      firstPageEnable: false,
+      prevPageEnable: false,
+      nextPageEnable: false,
+      lastPageEnable: false,
     };
   }
 
@@ -32,19 +49,23 @@ export default class ProfileSells extends PureComponent {
 
   componentDidMount() {
     const {jwtToken} = this.props;
-    fetch(`${serverAddress}/api/producer/orders`, {
+    fetch(`${serverAddress}/api/producer/orders?scope=pending`, {
       headers: {
         'Authorization': `Bearer ${jwtToken}`,
       },
     })
       .then(res => res.json())
       .then(res => {
+          const lastPage = res.result.pagination.last_page;
           this.setState(
             prevState => {
               return {
                 ...prevState,
                 orders: res.result,
                 itemsLoaded: true,
+                lastPage: lastPage,
+                nextPageEnable: lastPage > 1,
+                lastPageEnable: lastPage > 1,
               };
             }
           );
@@ -62,39 +83,217 @@ export default class ProfileSells extends PureComponent {
     this.props.getID(id);
   };
 
-  render() {
-    const { error, orders, itemsLoaded } = this.state;
+  handleChange = (event, value) => {
+    const {jwtToken} = this.props;
+    let scope;
 
-    if (error) {
-      return <p>Ошибка: {error.message}</p>;
+    switch (value) {
+      case 0:
+        scope = 'pending';
+        break;
+      case 1:
+        scope = 'close';
+        break;
+      case 2:
+        scope = 'reject';
+        break;
     }
-    else
-    if (!itemsLoaded) {
-      return <p className="load_info">Пожалуйста, подождите, идет загрузка страницы</p>;
-    }
-    else {
-      let content;
-      if (orders === undefined || orders.length === 0 || orders.orders === undefined || orders.orders.length === 0) {
-        content = <div className="load_info">
-          <div/>
-          <p>Вы еще не продали товар.</p>
-        </div>;
+
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          itemsLoaded: false,
+          value: value,
+        };
       }
-      else
-        content = (orders.orders.map((item, idx) => {
-            return (
-              <OrderItem item={item} key={idx} showOrderInfo={this.showOrderInfo}/>
-            );
-          }));
-      return (
-        <div className="seller_items">
-          <div className="seller_items_header">
-            <MyOrdersIcon className="my_orders_icon"/>
-            <h2>Заказы</h2>
-          </div>
-          {content}
+    );
+
+    fetch(`${serverAddress}/api/producer/orders?scope=${scope}`, {
+      headers: {
+        'Authorization': `Bearer ${jwtToken}`,
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+          const lastPage = res.result.pagination.last_page;
+          this.setState(
+            prevState => {
+              return {
+                ...prevState,
+                orders: res.result,
+                itemsLoaded: true,
+                currentPage: 1,
+                lastPage: lastPage,
+                firstPageEnable: false,
+                prevPageEnable: false,
+                nextPageEnable: lastPage > 1,
+                lastPageEnable: lastPage > 1,
+              };
+            }
+          );
+        },
+        error => {
+          this.setState({
+            itemsLoaded: true,
+            error,
+          });
+        });
+  };
+
+  changeList = page => {
+    const { value } = this.state;
+    const { jwtToken } = this.props;
+    let scope;
+
+    switch (value) {
+      case 0:
+        scope = 'pending';
+        break;
+      case 1:
+        scope = 'close';
+        break;
+      case 2:
+        scope = 'reject';
+        break;
+    }
+
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          itemsLoaded: false,
+        };
+      }
+    );
+    fetch(`${serverAddress}/api/producer/orders?scope=${scope}&page=${page}`, {
+      headers: {
+        'Authorization': `Bearer ${jwtToken}`,
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+          const lastPage = res.result.pagination.last_page;
+          this.setState(
+            prevState => {
+              return {
+                ...prevState,
+                currentPage: page,
+                lastPage: lastPage,
+                firstPageEnable: page > 1,
+                prevPageEnable: page > 1,
+                nextPageEnable: page < lastPage,
+                lastPageEnable: page < lastPage,
+                orders: res.result,
+                itemsLoaded: true,
+              };
+            }
+          );
+        },
+        error => {
+          this.setState({
+            itemsLoaded: true,
+            error,
+          });
+        });
+  };
+
+  render() {
+    const { error, orders, itemsLoaded, value, currentPage, lastPage, firstPageEnable, prevPageEnable, nextPageEnable, lastPageEnable } = this.state;
+    let content = '',
+      subcontent = '',
+      scope = '',
+      paginationButtons = '';
+
+    if (lastPage > 1) {
+      paginationButtons = (
+        <div className="pagination">
+          <IconButton
+            disabled={!firstPageEnable}
+            onClick={() => this.changeList(1)}
+          >
+            <FirstPage/>
+          </IconButton>
+          <IconButton
+            disabled={!prevPageEnable}
+            onClick={() => this.changeList(currentPage - 1)}
+          >
+            <PrevPage/>
+          </IconButton>
+          <span className="currentPage">
+                  {currentPage}
+                </span>
+          <IconButton
+            disabled={!nextPageEnable}
+            onClick={() => this.changeList(currentPage + 1)}
+          >
+            <NextPage/>
+          </IconButton>
+          <IconButton
+            disabled={!lastPageEnable}
+            onClick={() => this.changeList(lastPage)}
+          >
+            <LastPage/>
+          </IconButton>
         </div>
       );
     }
+    if (error) {
+      subcontent = <p className="load_info sell_orders_content">Ошибка: {error.message}</p>;
+    }
+    else
+      if (!itemsLoaded) {
+        subcontent = <p className="load_info sell_orders_content">Пожалуйста, подождите, идет загрузка страницы</p>;
+      }
+      else {
+        if (orders === undefined || orders.length === 0 || orders.orders === undefined || orders.orders.length === 0) {
+          switch (value) {
+            case 0:
+              scope = 'неотправленные';
+              break;
+            case 1:
+              scope = 'отпраленные';
+              break;
+            case 2:
+              scope = 'отмененные';
+              break;
+          }
+          subcontent = <div className="load_info sell_orders_content">
+            <div/>
+            <p>У Вас отсутствуют {scope} заказы.</p>
+          </div>;
+        }
+        else
+          subcontent = <div className="sell_orders_content">
+            {orders.orders.map((item, idx) => {
+              return (
+                <OrderItem item={item} key={idx} showOrderInfo={this.showOrderInfo}/>
+              );
+            })}
+          </div>;
+      }
+    content = (
+      <Fragment>
+        <Tabs
+          value={value}
+          onChange={this.handleChange}
+        >
+          <Tab label="Заказы для отправки"/>
+          <Tab label="Отправленные заказы"/>
+          <Tab label="Отмененные заказы"/>
+        </Tabs>
+        {subcontent}
+      </Fragment>
+    );
+    return (
+      <div className="seller_items">
+        <div className="seller_items_header">
+          <MyOrdersIcon className="my_orders_icon"/>
+          <h2>Заказы</h2>
+        </div>
+        {content}
+        {paginationButtons}
+      </div>
+    );
   }
 }

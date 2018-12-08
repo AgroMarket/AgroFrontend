61c73e75ea017b19cfd6dc0efd097835c3091a86
Alexandr Fokin <sfy@mail.ru>
2018-12-09 01:45:35 +0300

    пагинация покупателей

--- src/components/ProfileClients/ProfileClients.jsx
+++ src/components/ProfileClients/ProfileClients.jsx
@@ -2,6 +2,11 @@ import './ProfileClients.scss';
 
 import React, { PureComponent } from 'react';
 import MyOrdersIcon from '@material-ui/icons/DateRange';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPage from '@material-ui/icons/FirstPage';
+import PrevPage from '@material-ui/icons/ChevronLeft';
+import NextPage from '@material-ui/icons/ChevronRight';
+import LastPage from '@material-ui/icons/LastPage';
 import PropTypes from 'prop-types';
 
 import ProfileClient from 'components/ProfileClient';
@@ -19,6 +24,14 @@ export default class ProfileClients extends PureComponent {
       // Клиенты
       clients: {},
       itemsLoaded: false,
+      // текущая страница
+      currentPage: 1,
+      // последняя страница
+      lastPage: 1,
+      firstPageEnable: false,
+      prevPageEnable: false,
+      nextPageEnable: false,
+      lastPageEnable: false,
     };
   }
 
@@ -39,10 +52,65 @@ export default class ProfileClients extends PureComponent {
     })
       .then(res => res.json())
       .then(res => {
+        let lastPage;
+        if (res.result.pagination !== null)
+          lastPage = res.result.pagination.last_page;
+        else
+          lastPage = 1;
+        this.setState(
+          prevState => {
+            return {
+              ...prevState,
+              clients: res.result,
+              itemsLoaded: true,
+              lastPage: lastPage,
+              nextPageEnable: lastPage > 1,
+              lastPageEnable: lastPage > 1,
+            };
+          }
+        );
+      },
+      error => {
+        this.setState({
+          itemsLoaded: true,
+          error,
+        });
+      });
+  }
+
+  changeList = page => {
+    const { jwtToken } = this.props;
+
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          itemsLoaded: false,
+        };
+      }
+    );
+    fetch(`${serverAddress}/api/member/consumers?page=${page}`, {
+      headers: {
+        'Authorization': `Bearer ${jwtToken}`,
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+          let lastPage;
+          if (res.result.pagination !== null)
+            lastPage = res.result.pagination.last_page;
+          else
+            lastPage = 1;
           this.setState(
             prevState => {
               return {
                 ...prevState,
+                currentPage: page,
+                lastPage: lastPage,
+                firstPageEnable: page > 1,
+                prevPageEnable: page > 1,
+                nextPageEnable: page < lastPage,
+                lastPageEnable: page < lastPage,
                 clients: res.result,
                 itemsLoaded: true,
               };
@@ -55,40 +123,78 @@ export default class ProfileClients extends PureComponent {
             error,
           });
         });
-  }
+  };
 
   render() {
-    const {error, clients, itemsLoaded} = this.state;
-    const {itemHandle, getID } = this.props;
+    const { error, clients, itemsLoaded, currentPage, lastPage, firstPageEnable, prevPageEnable, nextPageEnable, lastPageEnable } = this.state;
+    const { itemHandle, getID } = this.props;
+    let content = '',
+      paginationButtons = '';
+
+    if (lastPage > 1) {
+      paginationButtons = (
+        <div className="pagination">
+          <IconButton
+            disabled={!firstPageEnable}
+            onClick={() => this.changeList(1)}
+          >
+            <FirstPage/>
+          </IconButton>
+          <IconButton
+            disabled={!prevPageEnable}
+            onClick={() => this.changeList(currentPage - 1)}
+          >
+            <PrevPage/>
+          </IconButton>
+          <span className="currentPage">
+            {currentPage}
+          </span>
+          <IconButton
+            disabled={!nextPageEnable}
+            onClick={() => this.changeList(currentPage + 1)}
+          >
+            <NextPage/>
+          </IconButton>
+          <IconButton
+            disabled={!lastPageEnable}
+            onClick={() => this.changeList(lastPage)}
+          >
+            <LastPage/>
+          </IconButton>
+        </div>
+      );
+    }
     if (error) {
-      return <p>Ошибка: {error.message}</p>;
+      content = <p className="orders_content">Ошибка: {error.message}</p>;
     }
     else if (!itemsLoaded) {
-      return <p className="load_info">Пожалуйста, подождите, идет загрузка страницы</p>;
+      content = <p className="load_info orders_content">Пожалуйста, подождите, идет загрузка страницы</p>;
     }
     else {
-      let content;
       if (clients === undefined || clients.length === 0 || clients.consumers === undefined || clients.consumers.length === 0) {
-        content = <div className="load_info">
+        content = <div className="load_info orders_content">
           <div/>
           <p>К сожалению у Вас еще не было покупателей.</p>
         </div>;
       }
       else
-        content = (clients.consumers.map((item, idx) => {
-          return (
-            <ProfileClient item={item} key={idx} itemHandle={itemHandle} getID={getID}/>
-          );
-        }));
-      return (
-        <div className="seller_items">
-          <div className="seller_items_header">
-            <MyOrdersIcon className="my_orders_icon"/>
-            <h2>Мои покупатели</h2>
-          </div>
-          {content}
-        </div>
-      );
+        content = <div className="orders_content">
+          {clients.consumers.map((item, idx) => {
+            return (
+              <ProfileClient item={item} key={idx} itemHandle={itemHandle} getID={getID}/>
+            );
+          })}
+        </div>;
     }
+    return (
+      <div className="seller_items">
+        <div className="seller_items_header">
+          <MyOrdersIcon className="my_orders_icon"/>
+          <h2>Мои покупатели</h2>
+        </div>
+        {content}
+        {paginationButtons}
+      </div>
+    );
   }
 }

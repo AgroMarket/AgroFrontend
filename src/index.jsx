4a68876e6dd118bf496187440cff6497b4eee400
Alexandr Fokin <sfy@mail.ru>
2018-12-09 19:11:37 +0300

    Roboto

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import './index.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from 'components/App';
-import './fonts/index.css';
 
 // Передаем корневой компонент App в DOM, инициализируем и визуализируем все дерево компонентов
 ReactDom.render(

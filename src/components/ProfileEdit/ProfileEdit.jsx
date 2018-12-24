eac15c41e6a9d1ca3e34b6a089e26416a424b845
Alexey T <alexx10@mail.ru>
2018-12-24 21:42:41 +0300

    added semicolons

--- src/components/ProfileEdit/ProfileEdit.jsx
+++ src/components/ProfileEdit/ProfileEdit.jsx
@@ -331,21 +331,21 @@ export default class ProfileEdit extends PureComponent {
                 {uploads.map(upload => {
                   switch (upload.state) {
                     case 'waiting':
-                      return <p key={upload.id}>Waiting to upload {upload.file.name}</p>
+                      return <p key={upload.id}>Waiting to upload {upload.file.name}</p>;
                     case 'uploading':
                       return (
                         <p key={upload.id}>
                           Uploading {upload.file.name}: {upload.progress}%
                         </p>
-                      )
+                      );
                     case 'error':
                       return (
                         <p key={upload.id}>
                           Error uploading {upload.file.name}: {upload.error}
                         </p>
-                      )
+                      );
                     case 'finished':
-                      return <p key={upload.id}>Finished uploading {upload.file.name}</p>
+                      return <p key={upload.id}>Finished uploading {upload.file.name}</p>;
                   }
                 })}
               </div>
